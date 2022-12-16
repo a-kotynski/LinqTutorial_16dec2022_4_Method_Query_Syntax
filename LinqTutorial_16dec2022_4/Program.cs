c0bfd43579e100cff13461e5d64ab0a2d1c0e4e5
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-16 09:21:38 +0100

    Add project files.

---
+++ LinqTutorial_16dec2022_4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LinqTutorial_16dec2022_4;
+//method syntax vs query syntax
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new[] { 4, 2, 7, 10, 12, 5, 4, 2 };
+        var smallOrderedNumbersMethodSyntax =
+            numbers
+            .Where(number => number < 10)
+            .OrderBy(number => number)
+            .Distinct();
+
+        Console.WriteLine(string.Join(", ", smallOrderedNumbersMethodSyntax));
+
+
+        var smallOrderedNumbersQuerySyntax =
+            (from number in numbers
+            where number < 10
+            orderby number
+            select number).Distinct();
+
+        Console.WriteLine(string.Join(", ", smallOrderedNumbersQuerySyntax));
+
+    }
+}
